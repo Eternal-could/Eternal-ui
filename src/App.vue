140608c02615263a11d084fa576809bf7a57a5b0
Eternal-could <20578466@qq.com>
2022-03-04 10:48:56 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <fir/>
+</template>
+
+<script>
+import fir from "./components/fir.vue";
+
+export default {
+  name: 'App',
+  components: {
+    fir
+  }
+}
+</script>
---
+++ src/components/fir.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    我的第一个组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: "fir"
+}
+</script>
+
+<style scoped>
+
+</style>
