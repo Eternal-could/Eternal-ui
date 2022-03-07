26bb7c54016e13d8b74e240dbf001a525fdc95cb
Eternal-could <20578466@qq.com>
2022-03-07 15:31:12 +0800

    下划线滑动

--- src/lib/TabsLib.vue
+++ src/lib/TabsLib.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="wheel-tabs">
-    <div class="wheel-tabs-nav">
+    <div class="wheel-tabs-nav" ref="container">
       <div
           class="wheel-tabs-nav-item"
           v-for="(t, index) in titles"
+          :ref="el => { if (el) navItems[index] = el }"
           @click="select(t)"
           :class="{selected: t === selected}"
           :key="index">{{t}}</div>
+      <div
+          class="wheel-tabs-nav-indicator"
+          ref="indicator"
+      ></div>
     </div>
     <div class="wheel-tabs-content">
       <component
@@ -21,7 +26,7 @@
 
 <script lang="ts">
 import TabLib from './TabLib.vue'
-import {computed} from 'vue';
+import {computed, onMounted, onUpdated, ref} from 'vue';
 export default {
   name: "TabsLib",
   props:{
@@ -31,6 +36,25 @@ export default {
     }
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([])
+    const indicator = ref<HTMLDivElement>(null)
+    const container = ref<HTMLDivElement>(null)
+    const x = () => {
+      const divs = navItems.value
+      const result = divs.filter(div=>div.classList.contains('selected'))[0]
+      const {width} = result.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+      const {left:left1} = container.value.getBoundingClientRect();
+      const {left:left2} = result.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left +'px';
+    }
+    onMounted(()=>{
+      x()
+    })
+    onUpdated(()=> {
+      x()
+    })
     const defaults = context.slots.default();
     defaults.forEach((tag)=> {
       if (tag.type !== TabLib) {
@@ -53,7 +77,10 @@ export default {
       defaults,
       titles,
       current,
-      select
+      select,
+      navItems,
+      indicator,
+      container
     }
   }
 }
@@ -68,6 +95,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -78,6 +106,15 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {
