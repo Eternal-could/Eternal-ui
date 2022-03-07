72faf48a5447c66d2cdbf968b3d69778e35c2f90
Eternal-could <20578466@qq.com>
2022-03-07 09:28:35 +0800

    Dialog组件支持visible

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,16 +1,26 @@
 <template>
   <h1>示例一</h1>
-  <DialogLib/>
+  <ButtonLib @click="toggle">toggle</ButtonLib>
+  <DialogLib :visible="x"/>
 </template>
 
-<script>
+<script lang="ts">
 import DialogLib from "../lib/DialogLib.vue";
+import ButtonLib from "../lib/ButtonLib.vue";
+import {ref} from 'vue';
 export default {
   name: "Dialog",
-  components: {DialogLib}
+  components: {DialogLib, ButtonLib},
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    return {x, toggle}
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss">
 
 </style>
