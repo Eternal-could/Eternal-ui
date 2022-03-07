549fc67dd361a6d1153230b2968ccd04bed74797
Eternal-could <20578466@qq.com>
2022-03-07 15:50:25 +0800

    优化3

--- src/lib/TabsLib.vue
+++ src/lib/TabsLib.vue
@@ -26,7 +26,7 @@
 
 <script lang="ts">
 import TabLib from './TabLib.vue'
-import {onMounted, onUpdated, ref} from 'vue';
+import {onMounted, ref, watchEffect} from 'vue';
 export default {
   name: "TabsLib",
   props:{
@@ -39,19 +39,15 @@ export default {
     const selectedItem = ref<HTMLDivElement>(null)
     const indicator = ref<HTMLDivElement>(null)
     const container = ref<HTMLDivElement>(null)
-    const x = () => {
-      const {width} = selectedItem.value.getBoundingClientRect()
-      indicator.value.style.width = width + 'px'
-      const {left:left1} = container.value.getBoundingClientRect();
-      const {left:left2} = selectedItem.value.getBoundingClientRect();
-      const left = left2 - left1;
-      indicator.value.style.left = left +'px';
-    }
     onMounted(()=>{
-      x()
-    })
-    onUpdated(()=> {
-      x()
+      watchEffect(()=>{
+        const {width} = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {left:left1} = container.value.getBoundingClientRect();
+        const {left:left2} = selectedItem.value.getBoundingClientRect();
+        const left = left2 - left1;
+        indicator.value.style.left = left +'px';
+      })
     })
     const defaults = context.slots.default();
     defaults.forEach((tag)=> {
