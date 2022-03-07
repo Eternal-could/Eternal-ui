1110abf156edf2c13e6e5ad5ebb30453724902e4
Eternal-could <20578466@qq.com>
2022-03-07 20:10:44 +0800

    引入图标

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="topNav">
-    <div class="logo">LOGO</div>
+    <div class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-lei"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -40,6 +44,10 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    > svg{
+      width: 42px;
+      height: 42px;
+    }
   }
 
   > .menu {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="topNavAndBanner">
-    <TopNav/>
-    <div class="banner">
-      <h1>轮子UI</h1>
-      <h2>一个简单的UI框架</h2>
-      <p class="actions">
-        <a href="https://github.com">GitHub</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+  <div>
+    <div class="topNavAndBanner">
+      <TopNav/>
+      <div class="banner">
+        <h1>轮子UI</h1>
+        <h2>一个简单的UI框架</h2>
+        <p class="actions">
+          <a href="https://github.com">GitHub</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
+    </div>
+    <div class="features">
+      <svg class="icon">
+        <use xlink:href="#icon-Vue"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-ts"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-dengta"></use>
+      </svg>
     </div>
   </div>
 </template>
@@ -24,7 +37,15 @@ export default {
 .topNavAndBanner{
   background-image: linear-gradient(to left top, #d16ba5, #c777b9, #ba83ca, #aa8fd8, #9a9ae1, #8aa7ec, #79b3f4, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1);
 }
+.features {
+  > svg {
+    height: 64px;
+    width: 64px;
+  }
+}
 .banner {
+  color: rgb(141,178,220);
+  text-shadow: 0.1em 0.1em 0.15em rgb(109, 49, 199);
   padding: 100px 0;
   display: flex;
   justify-content: center;
@@ -34,12 +55,14 @@ export default {
     padding: 8px 0;
     a {
       margin:0 8px;
-      background: #fff;
+      background: rgb(176, 135, 201,0.6);
       display: inline-block;
-      height: 28px;
-      line-height: 28px;
-      border-radius: 14px;
-      padding: 0 8px;
+      color: white;
+      border-radius: 4px;
+      padding: 8px 24px;
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
