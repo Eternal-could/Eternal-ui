8ac6c40dc88d044bbe75e6622b87e881817495a4
Eternal-could <20578466@qq.com>
2022-03-07 11:32:09 +0800

    渲染TagsLib

--- src/lib/TabsLib.vue
+++ src/lib/TabsLib.vue
@@ -1,13 +1,32 @@
 <template>
-  <h1>asd</h1>
+  <div>
+    <div v-for="(t, index) in titles" :key="index">{{t}}</div>
+    <component v-for="(c, index ) in defaults" :key="index" :is="c"/>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+import TabLib from './TabLib.vue'
 export default {
-  name: "TabsLib"
+  name: "TabsLib",
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag)=> {
+      if (tag.type !== TabLib) {
+        throw new Error('children component of TabsLib must be TabLib')
+      }
+    })
+    const titles = defaults.map((tag)=>{
+      return tag.props.title
+    })
+    return {
+      defaults,
+      titles
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
