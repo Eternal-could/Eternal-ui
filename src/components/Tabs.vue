ceb4698d5fdbdb7398ff06dad5a798c64a2b13a2
Eternal-could <20578466@qq.com>
2022-03-07 12:22:04 +0800

    使用css更新样式

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>示例一</h1>
-  <TabsLib>
+  <TabsLib v-model:selected="x">
     <TabLib title="导航1">内容1</TabLib>
     <TabLib title="导航2">内容2</TabLib>
   </TabsLib>
@@ -9,11 +9,16 @@
 <script lang="ts">
 import TabsLib from '../lib/TabsLib.vue'
 import TabLib from '../lib/TabLib.vue'
+import {ref} from 'vue';
 export default {
   name: "Tabs",
   components: {
     TabsLib,
-    TabLib
+    TabLib,
+  },
+  setup() {
+    const x = ref('导航1');
+    return {x}
   }
 }
 </script>
--- src/lib/TabsLib.vue
+++ src/lib/TabsLib.vue
@@ -1,14 +1,35 @@
 <template>
-  <div>
-    <div v-for="(t, index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c, index ) in defaults" :key="index" :is="c"/>
+  <div class="wheel-tabs">
+    <div class="wheel-tabs-nav">
+      <div
+          class="wheel-tabs-nav-item"
+          v-for="(t, index) in titles"
+          @click="select(t)"
+          :class="{selected: t === selected}"
+          :key="index">{{t}}</div>
+    </div>
+    <div class="wheel-tabs-content">
+      <component
+          class="wheel-tabs-content-item"
+          :class="{selected: c.props.title === selected}"
+          v-for="(c,index) in defaults"
+          :key="index"
+          :is="c"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import TabLib from './TabLib.vue'
+import {computed} from 'vue';
 export default {
   name: "TabsLib",
+  props:{
+    selected: {
+      type: Boolean,
+      default: true
+    }
+  },
   setup(props, context) {
     const defaults = context.slots.default();
     defaults.forEach((tag)=> {
@@ -16,17 +37,57 @@ export default {
         throw new Error('children component of TabsLib must be TabLib')
       }
     })
+    const current = computed(()=>{
+      console.log('review');
+      return  defaults.filter((tag)=>{
+        return  tag.props.title === props.selected
+      })[0]
+    })
     const titles = defaults.map((tag)=>{
       return tag.props.title
     })
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
     return {
       defaults,
-      titles
+      titles,
+      current,
+      select
     }
   }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.wheel-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
 </style>
