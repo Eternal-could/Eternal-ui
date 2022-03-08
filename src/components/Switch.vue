959e9fbbcb2dad9cbecc6b59af4c05751d393348
Eternal-could <20578466@qq.com>
2022-03-08 11:43:48 +0800

    抽离需要拷贝的代码

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,15 +1,42 @@
 <template>
   <div>
-    <SwitchLib v-model:value="bool"/>
+    <h1>Switch 组件示例 </h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <Switch1 />
+      </div>
+      <div class="demo-actions">
+        <ButtonLib>查看代码</ButtonLib>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+      </div>
+    </div>
+    <div class="demo">
+      <h2>支持 disabled </h2>
+      <div class="demo-component">
+        <Switch2/>
+      </div>
+      <div class="demo-actions">
+        <ButtonLib>查看代码</ButtonLib>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import SwitchLib from '../lib/SwitchLib.vue'
+import ButtonLib from '../lib/ButtonLib.vue'
+import Switch1 from './Switch1.vue'
+import Switch2 from './Switch2.vue'
 import {ref} from 'vue';
 export default {
   name: "Switch",
-  components: {SwitchLib},
+  components: {ButtonLib, SwitchLib, Switch1, Switch2},
   setup() {
     const bool = ref(false)
     return {bool}
@@ -17,6 +44,31 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  >h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    >pre {
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+      margin: 0;
+    }
+  }
+}
 </style>
---
+++ src/components/Switch2.vue
@@ -0,0 +1,21 @@
+<template>
+  <SwitchLib v-model:value="bool" disabled/>
+</template>
+
+<script lang="ts">
+import SwitchLib from '../lib/SwitchLib.vue';
+import {ref} from 'vue';
+
+export default {
+  name: "Switch1",
+  components: {SwitchLib},
+  setup() {
+    const bool = ref(false)
+    return {bool}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
